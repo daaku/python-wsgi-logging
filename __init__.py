358631e89856a550aa37e8da7490c6fb50b8437c
Naitik Shah <naitik.shah@yahoo.com>
2008-09-24 03:11:57 +0000

    basic logging by default, option to turn on more verbose logging

--- __init__.py
+++ __init__.py
@@ -1,15 +1,27 @@
-import pprint
+import logging
 
 class LoggingMiddleware:
-    def __init__(self, application):
-        self.__application = application
+    def __init__(self, application, full=False):
+        self._application = application
+        self._full = full
 
     def __call__(self, environ, start_response):
         errors = environ['wsgi.errors']
-        pprint.pprint(('REQUEST', environ), stream=errors)
 
-        def _start_response(status, headers):
-            pprint.pprint(('RESPONSE', status, headers), stream=errors)
-            return start_response(status, headers)
+        url = environ['wsgi.url_scheme'] + '://' + environ['HTTP_HOST'] + environ['PATH_INFO']
+        if environ['QUERY_STRING']:
+            url += '?' + environ['QUERY_STRING']
+        request_method = environ['REQUEST_METHOD']
+        logging.info(request_method + ' ' + url)
 
-        return self.__application(environ, _start_response)
+        if self._full:
+            import pprint
+            pprint.pprint(('REQUEST', environ), stream=errors)
+
+            def _start_response(status, headers):
+                pprint.pprint(('RESPONSE', status, headers), stream=errors)
+                return start_response(status, headers)
+
+            return self._application(environ, _start_response)
+        else:
+            return self._application(environ, start_response)
